893beab02ad21dfd4a09a79003d93aefa2af92fe
Lucas Teng <lucasteng@icloud.com>
2024-12-02 01:39:43 -0500

    Day 2

--- src/days/day2.ts
+++ src/days/day2.ts
@@ -1,11 +1,36 @@
 import { IDay, IPart } from '../internalTypes';
 
 const part1: IPart = (input) => {
-  return '';
+  const reports = input.split('\n').map(report => report.split(' ').map(Number));
+  const validReports = reports.filter(isSafe);
+
+  return validReports.length;
+};
+
+const isSafe = (level: number[]) => {
+  const differences: number[] = [];
+  for(let i = 1; i < level.length; i++) {
+    differences.push(level[i] - level[i-1]);
+  }
+  const increasing = differences.every(diff => diff >=1 && diff <= 3);
+  const decreasing = differences.every(diff => diff <=-1 && diff >= -3);
+
+  return increasing || decreasing;
 };
 
 const part2: IPart = (input) => {
-  return '';
+  const reports = input.split('\n').map(report => report.split(' ').map(Number));
+  const validReports = reports.filter(report=>{
+    if(isSafe(report)) return true;
+    for(let i = 0; i < report.length; i++) {
+      const newReport = report.filter((_, idx) => idx !== i);
+      if(isSafe(newReport)){
+        return true;
+      }
+    }
+    return false;
+  });
+  return validReports.length;
 };
 
 export const Day: IDay = {
--- test/day2.test.ts
+++ test/day2.test.ts
@@ -1,12 +1,19 @@
 import assert from 'assert';
 
-import { Day } from '../src/days/day0';
+import { Day } from '../src/days/day2';
 
-describe('Day 0', ()=>{
+const testinput = `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`;
+
+describe('Day 2', ()=>{
   it('should run part 1', ()=>{
-    assert.equal(Day.part1(''),'');
+    assert.equal(Day.part1(testinput),2);
   });
   it('should run part 2', ()=>{
-    assert.equal(Day.part2(''),'');
+    assert.equal(Day.part2(testinput),4);
   });
 });
