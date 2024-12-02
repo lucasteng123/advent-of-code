df7eaee40c92ec522d75570a2e31dac6d2deb58f
Lucas Teng <lucasteng@icloud.com>
2024-12-02 00:31:08 -0500

    Day 1

---
+++ test/day1.test.ts
@@ -0,0 +1,19 @@
+import assert from 'assert';
+
+import { Day } from '../src/days/day1';
+
+const testinput = `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`;
+
+describe('Day 1', ()=>{
+  it('should run part 1', ()=>{
+    assert.equal(Day.part1(testinput),11);
+  });
+  it('should run part 2', ()=>{
+    assert.equal(Day.part2(testinput),31);
+  });
+});
