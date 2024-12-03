1a8b89acdb3195823b779564d03a17357de059ac
Lucas Teng <lucasteng@icloud.com>
2024-12-03 00:37:57 -0500

    Day 3

---
+++ src/days/day3.ts
@@ -0,0 +1,39 @@
+import { IDay, IPart } from '../internalTypes';
+
+const part1: IPart = (input) => {
+  const corruptedMemory  = input.string;
+
+  const instructions = corruptedMemory.matchAll(/mul\((\d+),(\d+)\)/g);
+  let memory = 0;
+  for (const instruction of instructions) {
+    memory += parseInt(instruction[1]) * parseInt(instruction[2]);
+  }
+
+  return memory;
+};
+
+const part2: IPart = (input) => {
+  const corruptedMemory  = input.string;
+  let enabled = true;
+
+  let accumulator = 0;
+  
+  const parsedInstructions = corruptedMemory.matchAll(/(?<enable>do\(\)|don't\(\))|(?<command>mul\(\d+,\d+\))/g);
+
+  for (const instruction of parsedInstructions) {
+    if (instruction.groups?.enable) {
+      enabled = instruction.groups.enable === 'do()';
+    } else if (instruction.groups?.command && enabled) {
+      const [a, b] = instruction.groups.command.match(/\d+/g)!.map(Number);
+      accumulator += a * b;
+    }
+  }
+  
+  return accumulator;
+};
+
+export const Day: IDay = {
+  part1,
+  part2
+};
+
---
+++ test/day3.test.ts
@@ -0,0 +1,15 @@
+import assert from 'assert';
+
+import { Day } from '../src/days/day3';
+import { parseInput } from '../src/utils/input';
+
+const testInput = parseInput('xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))');
+
+describe.only('Day 3', ()=>{
+  it('should run part 1', ()=>{
+    assert.equal(Day.part1(testInput),161);
+  });
+  it('should run part 2', ()=>{
+    assert.equal(Day.part2(parseInput('xmul(2,4)&mul[3,7]!^don\'t()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))')),48);
+  });
+});
